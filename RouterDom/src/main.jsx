6a11a7465efd78d51e685966119d53f767e2f959
shibu kumar <shibukumar8892210005@gmail.com>
2024-03-19 22:57:35 +0530

    React Router Dom

--- RouterDom/src/main.jsx
+++ RouterDom/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Home from './Components/Home/Home.jsx'
 import About from './Components/About/About.jsx'
-
+import Contact from './Components/Contact/Contact.jsx'
+import Github from './Components/Github/Github.jsx'
+import User from './Components/User/User.jsx'
 
 const router=createBrowserRouter([
   {
@@ -20,13 +22,34 @@ const router=createBrowserRouter([
       path:"about/",
       element:<About/>
     },
+     {
+      path:"contact/",
+      element:<Contact/>
+    },
+     {
+      path:"github/",
+      element:<Github/>
+    },
+     {
+      path:"user/:userid",
+      element:<User/>
+    },
     ]
   }
 ])
 
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <route path="/" element={<App />} >
+//       <route path="" element={<Home />} />
+//       <route path="about" element={<About />} />
+//     </route>
+//   )
+// )
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
