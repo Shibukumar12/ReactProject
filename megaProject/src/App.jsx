da0aa09fc10629eeaa90d885ec2d74b4bb98dcdc
shibu kumar <shibukumar8892210005@gmail.com>
2024-04-03 22:29:28 +0530

    set the Appwrite Enviroment

---
+++ megaProject/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Conf from './conf/Conf';
+
+function App() {
+  
+  console.log(Conf.appwriteUrl);
+
+  return (
+    <>
+     <h1>mega project is to be ready</h1>
+    </>
+  )
+}
+
+export default App
