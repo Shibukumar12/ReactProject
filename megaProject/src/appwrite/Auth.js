553bb17863ad92cb636f9e0e6ab66603eccc1490
shibu kumar <shibukumar8892210005@gmail.com>
2024-04-11 21:36:54 +0530

    make the components

--- megaProject/src/appwrite/Auth.js
+++ megaProject/src/appwrite/Auth.js
@@ -51,22 +51,3 @@ export class Authservice{
 export const authservice= new Authservice()
 
 
-
-
-
-
-
-
-// const client = new Client()
-//     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-//     .setProject('<PROJECT_ID>');               // Your project ID
-
-// const account = new Account(client);
-
-// const promise = account.create('[USER_ID]', 'email@example.com', '');
-
-// promise.then(function (response) {
-//     console.log(response); // Success
-// }, function (error) {
-//     console.log(error); // Failure
-// });
--- megaProject/src/components/index.js
+++ megaProject/src/components/index.js
@@ -2,10 +2,14 @@ import Header from './header/Header'
 import Footer from './footer/Footer'
 import Container from './container/Container'
 import LogoutBtn from './header/LogoutBtn'
+import {Input}  from './Input'
+import Button from './Button'
 
 export {
     Header,
     Footer,
     Container,
+    Input,
     LogoutBtn,
+    Button
 } 
